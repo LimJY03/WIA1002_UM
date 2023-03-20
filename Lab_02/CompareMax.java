0d36e42404e79397f2a634aee0a4822e8f86ffd8
Lim JY <limjunyi03@gmail.com>
2023-03-20 06:26:14 +0000

    Create L2Q2 Solution

---
+++ Lab_02/CompareMax.java
@@ -0,0 +1,12 @@
+public class CompareMax {
+    
+    public static <T extends Comparable<T>> T maximum(T a, T b, T c) {
+        
+        T result = a;
+
+        if (c.compareTo(result) > 0) result = c;
+        else if (b.compareTo(result) > 0) result = b;
+        
+        return result;
+    }
+}
