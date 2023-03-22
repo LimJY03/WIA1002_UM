31d8260131093eb9b66d9ae678a9fc4f0020f54b
Lim JY <limjunyi03@gmail.com>
2023-03-22 03:39:32 +0000

    Update ArrayBag

--- Lab_03/ArrayBag.java
+++ Lab_03/ArrayBag.java
@@ -66,4 +66,32 @@ public class ArrayBag<T> implements BagInterface<T> {
         return false;
     }
     public T[] toArray() { return this.bag; }
+    public ArrayBag<T> union(ArrayBag<T> bag) {
+        
+        ArrayBag<T> newBag = new ArrayBag<>(this.bag.length + bag.toArray().length);
+        
+        for (int i = 0; i < this.getCurrentSize(); i++) newBag.add(this.bag[i]);
+
+        T[] bag2 = bag.toArray();
+
+        for (int i = 0; i < bag2.length; i++) newBag.add(bag2[i]);
+
+        return newBag;
+    }
+    public ArrayBag<T> interect(ArrayBag<T> bag) {
+
+        ArrayBag<T> newBag = new ArrayBag<>(this.bag.length + bag.toArray().length);
+
+        for (int i = 0; i < this.getCurrentSize(); i++) if (!newBag.contains(this.bag[i])) newBag.add(this.bag[i]);
+        
+        T[] bag2 = bag.toArray();
+
+        for (int i = 0; i < bag2.length; i++) if (!newBag.contains(bag2[i])) newBag.add(bag2[i]);
+
+        return newBag;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
 }
