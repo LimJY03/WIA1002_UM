4e936b7321bcac54b78f451f5c968d75c8bdbe62
Lim JY <limjunyi03@gmail.com>
2023-03-27 10:03:00 +0000

    Added Tester Class

---
+++ Lab_04/TestLinkedList.java
@@ -0,0 +1,44 @@
+public class TestLinkedList {
+    
+    public static void main(String[] args) {
+        
+        MyLinkedList<Character> ll = new MyLinkedList<>();
+        
+        // Append the following : a, b, c, d, e
+        ll.addLast('a');
+        ll.addLast('b');
+        ll.addLast('c');
+        ll.addLast('d');
+        ll.addLast('e');
+
+        // Print all the elements in the list
+        ll.print();
+
+        // Reverse all the elements in the list. 
+        ll.reverse();
+
+        // Retrieve the number of elements in the list.
+        ll.indexOf(ll.getLast());
+
+        // Retrieve the first and last value.
+        System.out.printf("First Element\n", ll.getFirst());
+        System.out.printf("Last Element\n", ll.getLast());
+
+        // Delete the third value.
+        System.out.println(ll.remove(2)); 
+
+        // Retrieve the index location for the second and third value. 
+        System.out.printf("Index Location for b: %s\n", ll.indexOf('b'));
+        System.out.printf("Index Location for c: %s\n", ll.indexOf('c'));
+
+        // Checks if the list has the value ‘c’.
+        System.out.printf("The list contains 'c'? %s\n", ll.contains('c'));
+
+        // Replace the items individually with the following: j,a,v,a.
+        ll.set(0, 'j');
+        ll.set(1, 'a');
+        ll.set(2, 'v');
+        ll.set(3, 'a');
+        ll.print();
+    }
+}
