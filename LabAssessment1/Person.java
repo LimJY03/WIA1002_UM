8f73754936ca6e16263d84944deaeebf8c5bcddf
Lim JY <limjunyi03@gmail.com>
2023-04-12 02:00:11 +0000

    Changed Folder Name

---
+++ LabAssessment1/Person.java
@@ -0,0 +1,15 @@
+public class Person {
+
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s (age=%s)", this.name, this.age);
+    }
+}   
