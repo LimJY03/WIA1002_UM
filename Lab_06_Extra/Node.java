1fee0e5e9eee5d066dfcd89aa41f56e87242056b
Lim JY <limjunyi03@gmail.com>
2023-04-19 02:08:11 +0000

    Create Node.java

---
+++ Lab_06_Extra/Node.java
@@ -0,0 +1,12 @@
+package Lab_06_Extra;
+
+public class Node {
+    
+    public int val;
+    public Node prev;
+    public Node next;
+    public Node child;
+
+    // Constructor
+    public Node(int val) { this.val = val; }
+}
